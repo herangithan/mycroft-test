80b642165d5dfddbc2c1d6d3d6e2c3079d5aacef
Herangithan Manoharan <herangithan@gmail.com>
2019-01-28 15:22:29 +0800

    test

--- __init__.py
+++ __init__.py
@@ -69,7 +69,7 @@ class BestSkill(MycroftSkill):
         p2 = "158195662888-u49u2d7mpdilq3sjq4h0dq0dto7j8uaf.apps.googleusercontent.com"
         p3 = "--device-model-id"
         p4 = "razer-project-razer_assistant_hub-wigbnw"
-        pro = subprocess.call(["/home/herangithan/env/bin/python3",path,p1,p2,p3,p4],stdout=subprocess.PIPE)
+        pro = subprocess.call(["/home/herangithan/env/bin/python3",path,p1,p2,p3,p4],stdout=self.speak_dialog(),stdin=subprocess.PIPE)
         #pro = subprocess.Popen("/home/herangithan/env/bin/python3 " +path + " " + p1 + " " + p2 + " " + p3+  " " + p4, shell=True, stdout = subprocess.PIPE,stdin = subprocess.PIPE, stderr=subprocess.PIPE)
         #out = pro.communicate()[0]
         #print(out)
